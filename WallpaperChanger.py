0ff51ea94414904db640bce95823a5134e61fd48
WebUwU <44942361+WebUwU@users.noreply.github.com>
2023-03-04 00:30:39 +0100

    Update WallpaperChanger.py

--- WallpaperChanger.py
+++ WallpaperChanger.py
@@ -1,7 +1,6 @@
 import os
 import requests
 import ctypes
-from urllib.parse import urlparse
 
 
 
@@ -23,21 +22,13 @@ temp_dir = os.environ['TEMP']
 
 url = input("Please enter the URL of the image: ")
 
-def get_valid_image_url():
-        if not (url.startswith("http://") or url.startswith("https://")):
-            print("Invalid URL. Please include 'http://' or 'https://' in the URL.")
-        parsed_url = urlparse(url)
-        if not parsed_url.netloc:
-            print("Invalid URL. Please enter a valid image URL.")
-        return url
-
-image_url = get_valid_image_url()
-
-
-
 response = requests.get(url)
+if response.status_code != 200:
+    print("Error: Invalid URL or unable to download image.")
+    exit()
+
 with open(f"{temp_dir}/myImage.png", 'wb') as f:
     f.write(response.content)
 
 SPI_SETDESKWALLPAPER = 20
-ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, f"{temp_dir}/myImage.png" , 0)
+ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, f"{temp_dir}/myImage.png", 0)
